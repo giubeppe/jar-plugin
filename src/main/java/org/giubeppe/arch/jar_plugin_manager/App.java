b70c5122e51ed72a98df4b1d50dd583b4e84a26c
Giuseppe Di Mauro <giubeppe@gmail.com>
2011-09-01 19:51:31 -0700

    adding to repository

---
+++ src/main/java/org/giubeppe/arch/jar_plugin_manager/App.java
@@ -0,0 +1,13 @@
+package org.giubeppe.arch.jar_plugin_manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
